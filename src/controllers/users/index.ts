464d1e09afc3305c15b3e91b1371c4cbca4220fb
Ruben Garcia Lopez <rugarcialopez@gmail.com>
2021-08-23 14:34:38 +0200

    CRUD users

---
+++ src/controllers/users/index.ts
@@ -0,0 +1,127 @@
+import { Response, Request } from 'express';
+import User, { IUser } from '../../models/user';
+import { IUserDocument } from '../../types/user';
+
+const getAllUsers = async (userId: string) => {
+  const users: IUser[] = await User.find({ _id: { $ne: userId }});
+  const transformedUsers =  (users || []).map((user: IUser) => (
+    {
+      id: user._id.toString(),
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      role: user.role,
+    }
+  ));
+  return transformedUsers;
+}
+
+const getUsers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    //Get the user ID from previous midleware
+    const userId = res.locals.jwtPayload.userId;
+    const role = req.query.role as string;
+    const filter = role ? { _id: { $ne: userId }, role: role} : { _id: { $ne: userId }};
+    const users: IUser[] = await User.find(filter);
+    const transformedUsers =  (users || []).map((user: IUser) => (
+      {
+        id: user._id.toString(),
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        role: user.role,
+      }
+    ));
+    res.status(200).json({ users: transformedUsers });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+const getUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const {
+        params: { id }
+    } = req;
+    const userDB = await User.findById({ _id: id });
+    if (!userDB) {
+      res.status(401).send({ message: 'User does not exist'});
+      return;
+    }
+    const user = {
+      id,
+      firstName: userDB.firstName,
+      lastName: userDB.lastName,
+      email: userDB.email,
+      role: userDB.role
+    }
+    res.status(200).json({user});
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+const addUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const userId = res.locals.jwtPayload.userId;
+    const body = req.body as Pick<IUserDocument, 'firstName' | 'lastName' | 'email' | 'password' | 'role'>;
+    //Check if firstName, lastName, email, password and role are set
+    const { firstName, lastName, email, password, role } = req.body;
+    if (!(firstName && lastName && email && password && role)) {
+      res.status(400).send({ message: 'fullName, email, password and role are required' });
+      return;
+    }
+
+    const userDB: IUser | null = await User.findOne({email: email.toLowerCase()});
+    if (userDB) {
+      res.status(401).send({ message: 'User already exists'});
+      return;
+    }
+
+    const user: IUser = new User({
+      firstName: body.firstName,
+      lastName: body.lastName,
+      email: body.email.toLowerCase(),
+      password: User.hashPassword(body.password),
+      role: body.role
+    });
+  
+    await user.save();
+
+    const users = await getAllUsers(userId);
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+const updateUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const userId = res.locals.jwtPayload.userId;
+    const {
+        params: { id },
+        body,
+    } = req;
+    await User.findByIdAndUpdate(
+      { _id: id },
+      body
+    );
+    const users = await getAllUsers(userId);
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+const deleteUser = async (req: Request, res: Response): Promise<void> => {
+  const userId = res.locals.jwtPayload.userId;
+  try {
+    await User.findByIdAndRemove(req.params.id);
+    const users = await getAllUsers(userId);
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+export { getUsers, getUser, addUser, updateUser, deleteUser };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,9 @@
 import { Router, Response, Request } from 'express';
+import { verifyAuthToken } from '../middlewares/verifyAuthToken';
+import { verifyRole } from '../middlewares/verifyRole';
+import { Role } from '../models/user';
 import { signIn, signUp } from '../controllers/auth/index';
+import { addUser, getUsers, updateUser, deleteUser } from '../controllers/users/index';
 
 const router: Router = Router();
 
@@ -8,6 +12,10 @@ router.get('/', (req: Request, res: Response): void => {
 });
 router.post('/signUp', signUp);
 router.post('/signIn', signIn);
+router.get('/users',[verifyAuthToken, verifyRole([Role.Manager])], getUsers);
+router.post('/add-user', [verifyAuthToken, verifyRole([Role.Manager])], addUser);
+router.put('/update-user/:id', [verifyAuthToken, verifyRole([Role.Manager])], updateUser);
+router.delete('/delete-user/:id', [verifyAuthToken, verifyRole([Role.Manager])], deleteUser);
 
 
 export default router;
